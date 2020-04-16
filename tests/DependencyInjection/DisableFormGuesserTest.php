2830cc96fb5f88a38a103ad3bacd91d6a6f94879
Red Forks <redforks@gmail.com>
2020-04-16 15:44:14 +0800

    Fix DisableFormGuesserTest namespace

--- tests/DependencyInjection/DisableFormGuesserTest.php
+++ tests/DependencyInjection/DisableFormGuesserTest.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Bungle\Framework\Tests\DependencyInjection;
+namespace Bungle\FrameworkBundle\Tests\DependencyInjection;
 
 use Bungle\Framework\Form\BungleFormTypeGuesser;
 use Bungle\FrameworkBundle\DependencyInjection\DisableFormGuesser;
