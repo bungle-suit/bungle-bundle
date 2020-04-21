93d0305e783753aba7e5a775df4b5a897a759798
Red Forks <redforks@gmail.com>
2020-04-21 21:23:11 +0800

    ListIDName Command

--- tests/DependencyInjection/BungleFrameworkExtensionTest.php
+++ tests/DependencyInjection/BungleFrameworkExtensionTest.php
@@ -18,6 +18,7 @@ use Bungle\Framework\StateMachine\STTLocator\ContainerSTTLocator;
 use Bungle\Framework\StateMachine\Vina;
 use Bungle\Framework\Tests\StateMachine\EventListener\FakeAuthorizationChecker;
 use Bungle\Framework\Twig\BungleTwigExtension;
+use Bungle\FrameworkBundle\Command\ListIDNameCommand;
 use Bungle\FrameworkBundle\DependencyInjection\BungleFrameworkExtension;
 use Bungle\FrameworkBundle\DependencyInjection\HighIDNameTranslatorPass;
 use Doctrine\Bundle\MongoDBBundle\ManagerRegistry;
@@ -166,6 +167,15 @@ final class BungleFrameworkExtensionTest extends TestCase
     {
         $gen = $this->container->get(CodeGenerator::class);
         self::assertInstanceOf(CodeGenerator::class, $gen);
+    }
+
+    public function testListIDNameCommand(): void
+    {
+        $this->container->set('cache.app', new ArrayAdapter());
+        (new HighIDNameTranslatorPass())->process($this->container);
+        self::addManagerRegistry();
+        $cmd = $this->container->get('bungle.command.list_id_names');
+        self::assertInstanceOf(ListIDNameCommand::class, $cmd);
     }
 
     private function addManagerRegistry(): ManagerRegistry
