caa27bcb2a3e95c2de10bfab9dff0768660c7e0d
Red Forks <redforks@gmail.com>
2020-04-21 20:17:37 +0800

    BungleBundle remove RegisterListenerPass

--- src/BungleFrameworkBundle.php
+++ src/BungleFrameworkBundle.php
@@ -13,7 +13,6 @@ use Bungle\FrameworkBundle\DependencyInjection\RegisterCodeGeneratorPass;
 use Bungle\FrameworkBundle\DependencyInjection\RegisterSTTPass;
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\EventDispatcher\DependencyInjection\RegisterListenersPass;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class BungleFrameworkBundle extends Bundle
@@ -33,9 +32,6 @@ final class BungleFrameworkBundle extends Bundle
         $container->registerForAutoconfiguration(STTInterface::class)
             ->addTag(RegisterSTTPass::STT_TAG);
 
-        // TODO: remove RegisterListenersPass(), RegisterListenersPass() add by
-        // FrameworkBundle
-        $container->addCompilerPass(new RegisterListenersPass());
         $container->addCompilerPass(new HighIDNameTranslatorPass());
         $container->addCompilerPass(new DisableFormGuesser(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 100);
         $container->addCompilerPass(new RegisterCodeGeneratorPass());
