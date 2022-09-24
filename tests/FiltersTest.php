a65525bd51d311e6b819bc347d0534525e537790
forks <redforks@gmail.com>
2022-09-24 16:49:29 +0800

    Filters::afterThatTime()

--- tests/FiltersTest.php
+++ tests/FiltersTest.php
@@ -4,14 +4,27 @@ declare(strict_types=1);
 
 namespace Bungle\FrameworkBundle\Tests;
 
+use Bungle\Framework\Ent\BasalInfoService;
 use Bungle\Framework\Filters;
 use DateTime;
 use DateTimeInterface;
+use Mockery;
 use Mockery\Adapter\Phpunit\MockeryTestCase;
 use stdClass;
 
 class FiltersTest extends MockeryTestCase
 {
+    private BasalInfoService&Mockery\MockInterface $basal;
+    private Filters $filters;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->basal = Mockery::mock(BasalInfoService::class);
+        $this->filters = new Filters($this->basal);
+    }
+
     /** @dataProvider isInstanceOfProvider */
     public function testIsInstanceOf($exp, $type, $obj): void
     {
@@ -26,6 +39,23 @@ class FiltersTest extends MockeryTestCase
             'not match' => [false, 'stdClass', new DateTime()],
             'interface' => [true, DateTimeInterface::class, new DateTime()],
             'base class' => [true, MockeryTestCase::class, $this],
+        ];
+    }
+
+    /** @dataProvider afterThatTimeProvider */
+    public function testAfterThatTime($exp, $t): void
+    {
+        $this->basal->expects('now')->andReturn(new DateTime($t));
+        $f = $this->filters->afterThatTime(new DateTime('2021-01-01'));
+        $this->assertSame($exp, $f());
+    }
+
+    public function afterThatTimeProvider()
+    {
+        return [
+            'before' => [false, '2020-12-31'],
+            'equal' => [true, '2021-01-01'],
+            'after' => [true, '2021-01-02'],
         ];
     }
 }
