73b22027642b94d6886362bd985547ea7d7e32f1
forks <redforks@gmail.com>
2023-02-04 10:44:19 +0800

    Implement MemoryLimiter

--- tests/Export/ExcelWriter/MemoryLimiterTest.php
+++ tests/Export/ExcelWriter/MemoryLimiterTest.php
@@ -6,11 +6,29 @@ namespace Bungle\FrameworkBundle\Tests\Export\ExcelWriter;
 
 use Bungle\Framework\Export\ExcelWriter\MemoryLimiter;
 use Mockery\Adapter\Phpunit\MockeryTestCase;
+use RuntimeException;
 
 class MemoryLimiterTest extends MockeryTestCase
 {
     public function testParseMemoryLimit(): void
     {
         self::assertIsInt(MemoryLimiter::parseMemoryLimit());
+    }
+
+    public function testNoMemoryPressure(): void
+    {
+        $this->expectNotToPerformAssertions();
+
+        $limit = new MemoryLimiter();
+        $limit->check();
+    }
+
+    public function testMemoryNotEnough(): void
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('导出数据太多，内存不足');
+
+        $limit = new MemoryLimiter(1024);
+        $limit->check();
     }
 }
